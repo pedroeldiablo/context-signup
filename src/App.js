e9cd732bd4f6e1878a7629c946d70326d2d2df92
Peter Williams <pedroeldiablo@yahoo.com>
2019-09-11 13:23:10 +0100

    create changeLanguage context and use within form component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import Navbar from './Navbar';
 import Form from './Form';
 import PageContent from './PageContent';
-import { ThemeProvider } from './contexts/ThemeContext'
+import { ThemeProvider } from './contexts/ThemeContext';
+import { LanguageProvider } from './contexts/LanguageContext';
 
 function App() {
   return (
       <ThemeProvider>
-        <PageContent>
-          <Navbar />
-          <Form />
-        </PageContent>
+        <LanguageProvider>
+          <PageContent>
+            <Navbar />
+            <Form />
+          </PageContent>
+        </LanguageProvider>
       </ThemeProvider>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -3,36 +3,60 @@ import { withStyles } from '@material-ui/core/styles';
 import styles from './styles/FormStyles';
 import { Paper, Avatar, Typography, Select, MenuItem, FormControl, InputLabel, Input, FormControlLabel, Checkbox, Button } from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOpenOutlined';
+import { LanguageContext } from './contexts/LanguageContext';
 
+const words = {
+    english: {
+        signIn: "Sign In",
+        email: "Email",
+        password: "Password",
+        remember: "Remember Me",
+    }, 
+    french: {
+        signIn: "Se Connecter",
+        email: "Adresse électronique",
+        password: "Mot de Passe",
+        remember: "Souviens-toi De Moi",
+    }, 
+    spanish: {
+        signIn: "Registrarse",
+        email: "Correo electrónico",
+        password: "Contraseña",
+        remember: "Recuérdame",
+    }
+}
 class Form extends Component {
+    static contextType = LanguageContext;
     render() {
+        const { language, changeLanguage } = this.context;
         const { classes } = this.props;
-        return (
+        const { signIn, email, password, remember } = words[language];
+         return (
             <main className={classes.main}>
                 <Paper className={classes.paper}>
                     <Avatar className={classes.avatar}>
                         <LockOutlinedIcon />
                     </Avatar>
-                    <Typography variant="h5">Sign In</Typography>
-                    <Select value="english">
+                    <Typography variant="h5">{signIn}</Typography>
+                    <Select value={language} onChange={changeLanguage}>
                         <MenuItem value="english">English</MenuItem>
-                        <MenuItem value="french">French</MenuItem>
-                        <MenuItem vlaue="spanish">Spanish</MenuItem>
+                        <MenuItem value="french">Français</MenuItem>
+                        <MenuItem value="spanish">Espanol</MenuItem>
                     </Select>
                     <form className={classes.form}>
                         <FormControl margin="normal" required fullWidth>
-                            <InputLabel htmlFor="email">Email</InputLabel>
+                            <InputLabel htmlFor="email">{email}</InputLabel>
                             <Input id="email" name="email" autoFocus />
 
                         </FormControl>
                         <FormControl margin="normal" required fullWidth>
-                            <InputLabel htmlFor="password">Password</InputLabel>
+                            <InputLabel htmlFor="password">{password}</InputLabel>
                             <Input id="password" name="password" autoFocus />
 
                         </FormControl>
                         <FormControlLabel 
                         control={<Checkbox color="primary"/>}
-                        label="Remember me"
+                        label={remember}
                         />
                         <Button 
                         variant="contained" 
@@ -41,7 +65,7 @@ class Form extends Component {
                         color="primary"
                         className={classes.submit}
                         >
-                            Sign In
+                            {signIn}
                         </Button>
 
                         
