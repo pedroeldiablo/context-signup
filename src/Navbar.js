f810ebfa1e20ecabf36dab1f31f2a692d7a8bbdf
Peter Williams <pedroeldiablo@yahoo.com>
2019-09-11 14:04:24 +0100

    using function as a child pattern to subscribe to a context

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,6 +3,7 @@ import { withStyles } from '@material-ui/core/styles';
 import { AppBar, Toolbar, IconButton, Typography, Switch, InputBase } from '@material-ui/core';
 import SearchIcon from "@material-ui/icons/Search";
 import { ThemeContext } from "./contexts/ThemeContext";
+import { LanguageContext } from './contexts/LanguageContext';
 
 import styles from './styles/NavBarStyles';
 
@@ -19,31 +20,36 @@ class Navbar extends Component {
         const { isDarkMode, toggleTheme } = this.context;
         const { classes } = this.props;
         return (
-            <div className={classes.root}>
-                <AppBar position="static" color={isDarkMode ? "default" : "primary"}>
-                    <Toolbar>
-                        <IconButton className={classes.menuButton} color="inherit">
-                            <span>🇫🇷</span>
-                        </IconButton>
-                        <Typography className={classes.title} variant="h6" color="inherit">
-                            App Title
-                        </Typography>
-                        <Switch onChange={ toggleTheme }/>
-                        <div className={classes.grow} />
-                        <div className={classes.search}>
-                            <div className={classes.searchIcon}>
-                                <SearchIcon />
-                            </div>
-                            <InputBase placeholder="Search..." classes={{
-                                root: classes.inputRoot,
-                                input: classes.inputInput
-                            }} />
-                        </div>
-                        
-                    </Toolbar>
-                </AppBar>
-
-            </div>
+            <LanguageContext.Consumer>
+                {value => (
+                     <div className={classes.root}>
+                     <AppBar position="static" color={isDarkMode ? "default" : "primary"}>
+                         <Toolbar>
+                             <IconButton className={classes.menuButton} color="inherit">
+                                 <span role="img" aria-label="country flag">🇫🇷</span>
+                             </IconButton>
+                             <Typography className={classes.title} variant="h6" color="inherit">
+                                 App Title {value.language}
+                             </Typography>
+                             <Switch onChange={ toggleTheme }/>
+                             <div className={classes.grow} />
+                             <div className={classes.search}>
+                                 <div className={classes.searchIcon}>
+                                     <SearchIcon />
+                                 </div>
+                                 <InputBase placeholder="Search..." classes={{
+                                     root: classes.inputRoot,
+                                     input: classes.inputInput
+                                 }} />
+                             </div>
+                             
+                         </Toolbar>
+                     </AppBar>
+     
+                 </div>
+                )}
+            </LanguageContext.Consumer>
+           
         );
     }
 }
